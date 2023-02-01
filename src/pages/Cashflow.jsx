77965ab5f7d19d037c404e90ea92d22610074ba6
Shani Penkar <shanipenkar@gmail.com>
2023-02-01 11:33:03 +0200

    delete logic fixed

--- src/pages/Cashflow.jsx
+++ src/pages/Cashflow.jsx
@@ -38,6 +38,15 @@ const Cashflow = () => {
     });
   };
 
+  const handleDelete = (transactionId) => {
+    console.log(transactionId);
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: transactionId
+    })
+    console.log(state);
+  };
+
   return (
     <div className="px-6 py-10">
       <h1 className="flex justify-center font-extrabold text-5xl pb-2 text-light">
@@ -54,46 +63,50 @@ const Cashflow = () => {
 
       <div className="pt-7">
         {state.transactions.length === 0 ? (
-          <p className="text-center text-3xl text-light">
-            No transactions yet
-          </p>
+          <p className="text-center text-3xl text-light">No transactions yet</p>
         ) : (
           <div className="rounded-xl bg-white overflow-hidden">
-          <table className="justify-center border-secondary table-auto w-full text-center">
-            <thead className="bg-transparent text-black">
-              <tr className="text-sm font-medium">
-                <th className="border-secondary w-32">Date</th>
-                <th className="border-secondary">Name</th>
-                <th className="border-secondary">Type</th>
-                <th className="border-secondary w-32">Amount (₪)</th>
-                <th className="border-secondary">Category</th>
-                <th className="border-secondary">Description</th>
-                <th className="border-secondary">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.transactions.map((transaction) => (
-                <tr key={transaction.id} className="text-sm font-medium bg-gray-100">
-                
-                  <td className="border-secondary">{transaction.date}</td>
-                  <td className="border-secondary">{transaction.name}</td>
-                  <td className="border-secondary">{transaction.type}</td>
-                  <td className="border-secondary">{transaction.amount}</td>
-                  <td className="border-secondary">{transaction.category}</td>
-                  <td className="border-secondary">
-                    {transaction.description}
-                  </td>
-                  <td className="border-secondary"></td>
+            <table className="justify-center border-secondary table-auto w-full text-center">
+              <thead className="bg-transparent text-black">
+                <tr className="text-sm font-medium">
+                  <th className="border-secondary w-32">Date</th>
+                  <th className="border-secondary">Name</th>
+                  <th className="border-secondary">Type</th>
+                  <th className="border-secondary w-32">Amount (₪)</th>
+                  <th className="border-secondary">Category</th>
+                  <th className="border-secondary">Description</th>
+                  <th className="border-secondary">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {state.transactions.map((transaction) => (
+                  <tr
+                    key={transaction.id}
+                    className="text-sm font-medium bg-gray-100"
+                  >
+                    <td className="border-secondary">{transaction.date}</td>
+                    <td className="border-secondary">{transaction.name}</td>
+                    <td className="border-secondary">{transaction.type}</td>
+                    <td className="border-secondary">{transaction.amount}</td>
+                    <td className="border-secondary">{transaction.category}</td>
+                    <td className="border-secondary">
+                      {transaction.description}
+                    </td>
+                    <td className="border-secondary">
+                      <span className="cursor-pointer" onClick={()=> {handleDelete(transaction.id)}}>
+                        <i className="hover:text-primary fa-solid fa-trash text-black" />
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         )}
         <div className=" pt-5 text-center text-xl text-light">
-        <p>Total Expenses: {TotalExpenses}</p>
-        <p>Total Incomes: {TotalIncomes}</p>
-        <p>Balance: {TotalIncomes-TotalExpenses}</p>
+          <p>Total Expenses: {TotalExpenses}</p>
+          <p>Total Incomes: {TotalIncomes}</p>
+          <p>Balance: {TotalIncomes - TotalExpenses}</p>
         </div>
       </div>
     </div>
